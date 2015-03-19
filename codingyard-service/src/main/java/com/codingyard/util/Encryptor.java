b25f92ab3e8629d155ac4dfd493b844588b79ac1
Seulgi Kim <seulgi.kim.56@outlook.com>
2015-03-18 21:02:30 -0700

    Add Encryptor using jasypt

---
+++ codingyard-service/src/main/java/com/codingyard/util/Encryptor.java
@@ -0,0 +1,17 @@
+package com.codingyard.util;
+
+import org.jasypt.util.password.PasswordEncryptor;
+import org.jasypt.util.password.StrongPasswordEncryptor;
+
+public class Encryptor {
+
+    private static final PasswordEncryptor ENCRYPTOR = new StrongPasswordEncryptor();
+
+    public static String encrypt(final String plain) {
+        return ENCRYPTOR.encryptPassword(plain);
+    }
+
+    public static boolean isSame(final String plain, final String encrypted) {
+        return ENCRYPTOR.checkPassword(plain, encrypted);
+    }
+}
---
+++ codingyard-service/src/test/java/com/codingyard/util/EncryptorTest.java
@@ -0,0 +1,15 @@
+package com.codingyard.util;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class EncryptorTest {
+
+    private final String password = "safe_password_YolO_123_#$!";
+
+    @Test
+    public void plainPasswordShouldMatchItsEncryptedVersion() throws Exception {
+        assertTrue(Encryptor.isSame(password, Encryptor.encrypt(password)));
+    }
+}
