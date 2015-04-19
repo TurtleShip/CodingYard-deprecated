451c884aadbbbd2a8ac07615116f8b68d39a64fd
Seulgi Kim <seulgi.kim.56@outlook.com>
2015-04-19 16:49:01 -0700

    Added angular and bootstrap library.

---
+++ codingyard-frontend/src/main/resources/angularApp/app/app.module.js
@@ -0,0 +1,10 @@
+(function() {
+    var app = angular.module('codingyard', []);
+    app.controller('CodingyardController', function() {
+        this.seulgi = {
+            "firstName": "Seulgi",
+            "lastName": "Kim",
+            "age": 3
+        };
+    });
+})();
