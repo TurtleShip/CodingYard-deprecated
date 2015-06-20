a8dd9d6bd1360f0dcd5657455791b2e4fde35a80
Seulgi Kim <seulgi.kim.56@outlook.com>
2015-06-20 13:35:24 -0700

    Make search pretty.

--- frontend/app/components/topcoder/topcoder_view_controller.js
+++ frontend/app/components/topcoder/topcoder_view_controller.js
@@ -8,22 +8,37 @@
             $scope.message = null;
             $scope.error = null;
 
+            $scope.supported = {
+                divisions: {
+                    'Any': '',
+                    'Div 1': 'DIV1',
+                    'Div 2': 'DIV2'
+                },
+                difficulties: {
+                    Any: '',
+                    easy: 'EASY',
+                    medium: 'MEDIUM',
+                    hard: 'HARD'
+                },
+                languages: {
+                    Any: '',
+                    Java: 'JAVA',
+                    Cpp: 'CPP',
+                    C: 'C',
+                    Python: 'PYTHON',
+                    Ruby: 'RUBY',
+                    Other: 'OTHER'
+                }
+            };
+
             $scope.criteria = {
-                division: null,
-                difficulty: null,
-                problem_number: null,
-                language: null,
-                author_username: null
-            };
+                division: '',
+                difficulty: '',
+                problem_number: '',
+                language: '',
+                author_username: ''
 
-            $scope.available = {
-                divisions: {},
-                difficulties: {},
-                problem_numbers: {},
-                languages: {},
-                author_usernames: {}
             };
-
             $scope.solutionToDelete = null;
 
             $scope.deleteSolution = function (solution) {
@@ -54,36 +69,6 @@
                 modalInstance.result.then($scope.deleteSolution);
             };
 
-
-
-            $scope.populateAvailable = function (solutions) {
-
-                solutions.forEach(function (solution) {
-                    if (!(solution.division in $scope.available.divisions)) {
-                        $scope.available.divisions[solution.division] = true;
-                    }
-                    if (!(solution.difficulty in $scope.available.difficulties)) {
-                        $scope.available.difficulties[solution.difficulty] = true;
-                    }
-                    if (!(solution.problem_number in $scope.available.problem_numbers)) {
-                        $scope.available.problem_numbers[solution.problem_number] = true;
-                    }
-
-                    if (!(solution.language in $scope.available.languages)) {
-                        $scope.available.languages[solution.language] = true;
-                    }
-                    if (!(solution.author.username in $scope.available.author_usernames)) {
-                        $scope.available.author_usernames[solution.author.username] = true;
-                    }
-                });
-
-                $scope.available.divisions = Object.keys($scope.available.divisions);
-                $scope.available.difficulties = Object.keys($scope.available.difficulties);
-                $scope.available.problem_numbers = Object.keys($scope.available.problem_numbers);
-                $scope.available.languages = Object.keys($scope.available.languages);
-                $scope.available.author_usernames = Object.keys($scope.available.author_usernames);
-            };
-
             $scope.aceOption = {
                 mode: "text",
                 useWrapMode: true,
@@ -94,17 +79,17 @@
                         if ($scope.pickedSolution) {
                             _ace.getSession().setMode("ace/mode/" + AceEditor.getMode($scope.pickedSolution.language));
                         }
-
                     };
                 }
             };
 
             $scope.getSolutions = function () {
-                TopCoder.findAll($scope.criteria,
+                TopCoder.findAll({},
                     function (response) { // success
                         $scope.solutions = response;
                         $scope.solutions.forEach(function (solution) {
                             solution.canDelete = false;
+                            solution.author_name = solution.author.username;
                             SolutionPermission.canDelete({
                                     id: solution.id,
                                     contest: 'TOP_CODER'
@@ -119,7 +104,6 @@
                                 break;
                             default:
                                 $scope.message = "There are " + $scope.solutions.length + " solutions matching the given solution.";
-                                $scope.populateAvailable(response);
                         }
                     },
                     function () { // error
